597ad4c5700f07e3873c54d961b9dcb0136c1888
TYama <alohaty81@gmail.com>
2022-02-10 11:58:14 -0700

    problem6,7,8,9 are done

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -95,15 +95,25 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
-
+let someNum = myFavoriteNumbers[4]
+console.log(`${someNum}`)
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+//Subscripting (accessing values using their index) an array can fail. 
+//What happens if you subscript to the 7th element, but there are not 7 elements in the array? 
+//Let's write some code to check for that. 
+//Use an if statement to check the length of the 'myFavoriteNumbers' array. 
+//If it's less than 7, log 'There are not enough elements in this array' to the console. 
+//If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array.
+//(Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
-
+if(myFavoriteNumbers.length < 7){
+  console.log(`There are not enough elements in this array`)
+}else{
+  someNum = myFavoriteNumbers[6]
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -111,10 +121,15 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. 
+//If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
+for(let i = 0; i < listOfNumbers.length; i++){
+  if(listOfNumbers[i] % 3 === 0){
+    console.log(`${listOfNumbers[i]} is divisible by 3.`)
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -126,7 +141,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for(let i = letters.length - 1; i >= 0; i--){
+  console.log(`${letters[i]}`)
+}
 
 ////////// Advanced Problems //////////
 
